82a2ea82627efda83a67e97c593253a4c2201fc9
pipin <Aleksejsoronov3@gmail.com>
2024-08-08 22:08:13 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -4,6 +4,6 @@ age = 19
 print("Age:", str(age))
 age = age + 1
 print("New age:", str(+age))
-Is_student = (2)
-equ_al = (1)
-print("Is student:", Is_student>equ_al)
+Is_student = True
+print('It student:',Is_student)
+
